6abee57bae3f079de3f059491b0e9c35164b55f3
Nathan Lin <nathanlukelin@gmail.com>
2020-03-24 16:45:13 -0700

    Create Simple.java

---
+++ Simple.java
@@ -0,0 +1,15 @@
+public class Simple {
+
+	// Write a function that returns the smaller of
+	// two integer inputs
+	public static int min(int x, int y)
+	{
+		// YOUR CODE HERE
+	}
+
+	// Write a function that prints out "Hello, world!"
+	public static void helloWorld()
+	{
+		// YOUR CODE HERE
+	}
+}
